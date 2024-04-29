a9cb62d8280fc18f5dc96bab7ab6bfa1c56f345e
Ehtisham86 <ehtisham010101@gmail.com>
2024-04-30 00:26:39 +0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answer: { sentence: string } = await inquirer.prompt({
+  name: "sentence",
+  type: "input",
+  message: chalk.yellow("Enter your sentence to count the word:"),
+});
+
+const wordCount = answer.sentence.trim().split(" ");
+
+console.log(wordCount);
+console.log(
+  chalk.blueBright(`your sentence word count is: ${wordCount.length}`)
+);
